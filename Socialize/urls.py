9ecc58c3f394c229ef21024736354da0fe0c7d1d
Simarjeet Singh <simar.feb99@gmail.com>
2020-08-17 21:30:27 +0530

    first commit

---
+++ Socialize/urls.py
@@ -0,0 +1,32 @@
+"""simplesocial URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')) 
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    #path('', views.HomePage.as_view(), name="home"), 
+    #path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"),name='login'),
+    path('', auth_views.LoginView.as_view(template_name="accounts/login.html"),name='home'),
+    path('admin/', admin.site.urls),
+    path('test/', views.TestPage.as_view(), name="test"),
+    path('thanks/', views.ThanksPage.as_view(), name="thanks"),
+    path('accounts/', include("accounts.urls", namespace="accounts")),
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('posts/', include("posts.urls", namespace="posts")),
+    path('groups/',include("groups.urls", namespace="groups")),
+]
---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('signup/', views.SignUp.as_view(), name="signup"),
+]
