f1d7b2f6e42d733a8b687d322a5594fd011ed625
i-am-simar <36666700+i-am-simar@users.noreply.github.com>
2020-09-26 14:06:32 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 
 # Create your models here
 class User(auth.models.User, auth.models.PermissionsMixin):
+    """
+        Using auth user model for handling users
+    """
 
     def __str__(self):
         return "@{}".format(self.username)
